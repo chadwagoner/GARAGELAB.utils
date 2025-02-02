24e560d72582db6ab83e7134007ea8f74389032b
Chad <chawagon03@gmail.com>
2025-02-02 14:49:49 -0500

    UPDATE: alpine-linux/install-base.sh

--- alpine-linux/install-base.sh
+++ alpine-linux/install-base.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
 ### SET APK REPOSITORIES
-doas sed -i '1d' /etc/apk/repositories
-doas sed -i 's/#//g' /etc/apk/repositories
+doas curl -sL -o /etc/apk/repositories -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/alpine-linux/templates/apk/repositories
+
+### SET ROOT CRONTAB
+doas curl -sL -o /etc/crontabs/root -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/alpine-linux/templates/crontab/root
+
+### DISABLE IPV6 NETWORKING
+echo -e "net.ipv6.conf.all.disable_ipv6 = 1" | doas tee /etc/sysctl.d/99-system.conf >/dev/null
+
+### UPDATE OS
+doas apk upgrade -U --quiet
 
 ### INSTALL BASE PACKAGES
-doas apk add -U curl vim --quiet
+doas apk add -U curl htop vim --quiet
+
+### INSTALL PATCHWORK
+doas curl -sL -o /etc/periodic/monthly/patchwork -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/alpine-linux/templates/periodic/monthly/patchwork
+doas chmod 755 /etc/periodic/monthly/patchwork
