8c6aab9fae6dc5323e46b717ad8fab321759f613
Chad <chawagon03@gmail.com>
2025-02-02 14:50:50 -0500

    UPDATE: docker/portainer

--- docker/portainer/install.sh
+++ docker/portainer/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-### MAKE SERVICE DIRECTORY
+### MAKE PORTAINER DIRECTORY
 mkdir -p /opt/services/portainer
 mkdir -p /opt/services/portainer/data
 
 ### GET COMPOSE FILE
 curl -sL -o /opt/services/portainer/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/portainer/compose.yaml
 
-### START SERVICE
+### START PORTAINER
 docker compose -f /opt/services/portainer/compose.yaml up -d
