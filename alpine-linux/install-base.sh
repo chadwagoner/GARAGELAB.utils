7051bdc43d83c776d241370f174fbca1b9a60c8a
Chad <chawagon03@gmail.com>
2025-02-02 13:05:50 -0500

    ADD: alpine-linux/install-base.sh

---
+++ alpine-linux/install-base.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+### SET APK REPOSITORIES
+doas sed -i '1d' /etc/apk/repositories
+doas sed -i 's/#//g' /etc/apk/repositories
+
+### INSTALL BASE PACKAGES
+doas apk add -U curl vim --quiet
