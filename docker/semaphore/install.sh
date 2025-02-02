43b0fe8367a7c542c4ba0a3219762d7ab710e108
Chad <chawagon03@gmail.com>
2025-02-02 14:50:59 -0500

    UPDATE: docker/semaphore

--- docker/semaphore/install.sh
+++ docker/semaphore/install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-### MAKE SERVICE DIRECTORY
+### MAKE SEMAPHORE DIRECTORY
 mkdir -p /opt/services/semaphore
 mkdir -p /opt/services/semaphore/config
 mkdir -p /opt/services/semaphore/data
 
+### SET PERMISSIONS FOR SEMAPHORE USER
+doas chown 1001:1001 /opt/services/semaphore/config
+doas chown 1001:1001 /opt/services/semaphore/data
+
 ### GET COMPOSE FILE
 curl -sL -o /opt/services/semaphore/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/semaphore/compose.yaml
 
-### START SERVICE
+### START SEMAPHORE
 docker compose -f /opt/services/semaphore/compose.yaml up -d
