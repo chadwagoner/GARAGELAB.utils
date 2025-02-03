6e1fac8fe37f7597fac7a11e58abf0a7ca64ae72
Chad <chawagon03@gmail.com>
2025-02-03 00:54:07 -0500

    EDIT: alpine-linux/configure-nfs-media.sh

--- alpine-linux/configure-nfs-media.sh
+++ alpine-linux/configure-nfs-media.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# NOTE: THIS DOESN'T WORK AS INTENDED, NO USER INPUT AND BAD ENTRY TO FSTAB
+
 ### USER INPUT VARIABLES
 read -p 'NFS SERVER IP ADDRESS: ' nfs_server
 read -p 'NFS VOLUME: ' nfs_volume
 
 ### CREATE MEDIA DIRECTORY
 doas mkdir -p /mnt/media
+doas chown alpine:alpine /mnt/media
 
 ### UPDATE FSTAB
 echo -e "$nfs_server:$nfs_volume /mnt/media nfs rw,vers=4,rsize=32768,wsize=32768,soft 0 0" | doas tee -a /etc/fstab >/dev/null
