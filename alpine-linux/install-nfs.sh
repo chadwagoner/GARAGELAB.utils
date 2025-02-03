4910c17f4f6c5ad3d2acc42424616a8da00a726b
Chad <chawagon03@gmail.com>
2025-02-03 00:54:48 -0500

    EDIT: alpine-linux/install-nfs.sh

--- alpine-linux/install-nfs.sh
+++ alpine-linux/install-nfs.sh
@@ -4,7 +4,7 @@
 doas apk add -U nfs-utils --quiet
 
 ### ENABLE BOOT START
-doas rc-update add nfsmount boot
+doas rc-update add nfsmount boot >/dev/null 2>&1
 
 ### START SERVICE
-doas rc-service nfsmount start
+doas rc-service nfsmount start >/dev/null 2>&1
