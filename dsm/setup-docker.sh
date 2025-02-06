2de17fc0397d83ab1075b6797b7bf2862911a935
Chad <chawagon03@gmail.com>
2025-02-06 16:04:44 -0500

    RENAME: dsm/install-docker-compose.sh to dsm/setup-docker.sh

--- dsm/setup-docker.sh
+++ dsm/setup-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+### CREATE DEFAULT DOCKER NETWORKS
+docker network create db >/dev/null 2>&1
+docker network create internal >/dev/null 2>&1
+docker network create management >/dev/null 2>&1
+docker network create proxy >/dev/null 2>&1
+
 ### INSTALL DOCKER COMPOSE
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
