b17ef047ae89d812e97ea6487cf5481650f8b3e9
Chad <chawagon03@gmail.com>
2025-02-06 16:04:54 -0500

    ADD: docker/portainer-agent

---
+++ docker/portainer-agent/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### USER INPUT VARIABLES
+read -p 'SERVICE PATH: ' service_path < /dev/tty
+read -p 'DEFAULT DOCKER VOLUME PATH: ' docker_volume_path < /dev/tty
+
+### MAKE PORTAINER-AGENT DIRECTORY
+mkdir -p $service_path/portainer-agent
+
+### CREATE .ENV FOR DOCKER VOLUME PATH
+echo -e "VOLUME_PATH: \"$docker_volume_path\"" | sudo tee $service_path/portainer-agent/.env
+
+### GET COMPOSE FILE
+curl -sL -o $service_path/portainer-agent/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/portainer-agent/compose.yaml
+
+### START PORTAINER-AGENT
+# docker compose -f $service_path/portainer-agent/compose.yaml up -d
