925f6473b93062503fb6ee7080624c7e4b5a5d3b
Chad <chawagon03@gmail.com>
2025-02-07 11:13:54 -0500

    EDIT: alpine-linux/proxmox-install-qemu-guest-agent.sh

--- alpine-linux/proxmox-install-qemu-guest-agent.sh
+++ alpine-linux/proxmox-install-qemu-guest-agent.sh
@@ -6,5 +6,8 @@ doas apk add -U qemu-guest-agent --quiet
 ### ENABLE BOOT START
 doas rc-update add qemu-guest-agent default >/dev/null 2>&1
 
+### ANNOUCEMENT
+echo -e "MAKE SURE TO ENABLE QEMU-GUEST-AGENT IN PROXMOX..."
+
 ### POWEROFF TO ENABLE QEMU GUEST AGENT ON PROXMOX
 doas poweroff
