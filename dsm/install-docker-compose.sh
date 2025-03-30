c71f2dfc50e7af972090a6c1c3a313f3dacdbcb3
Chad <chawagon03@gmail.com>
2025-03-29 20:15:24 -0400

    refactor(dsm): docker setup

---
+++ dsm/install-docker-compose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### INSTALL DOCKER COMPOSE
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod 755 $DOCKER_CONFIG/cli-plugins/docker-compose
--- dsm/setup-docker.sh
+++ dsm/setup-docker.sh
@@ -5,9 +5,3 @@ docker network create db >/dev/null 2>&1
 docker network create internal >/dev/null 2>&1
 docker network create management >/dev/null 2>&1
 docker network create proxy >/dev/null 2>&1
-
-### INSTALL DOCKER COMPOSE
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod 755 $DOCKER_CONFIG/cli-plugins/docker-compose
