243b0b51643255771e161166249d8aaf310356a8
Chad <chawagon03@gmail.com>
2025-04-28 21:48:13 -0400

    feat(dsm): add scripts for checking and upgrading DSM and its packages

---
+++ dsm/install-patchwork.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### UPDATE CACHE AND FIND UPDATES
+# synopkg checkupdateall
+
+### UPGRADE APPS
+# synopkg upgradeall
---
+++ dsm/upgrade-dsm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# sudo synoupgrade --check | wc -l
+
+# if > 1
+# sudo synoupgrade --download
+# sudo synoupgrade --start
+
+# https://www.going-flying.com/blog/upgrading-synology-dsm-from-the-command-line.html
