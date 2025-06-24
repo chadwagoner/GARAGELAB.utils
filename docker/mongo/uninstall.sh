f1b560a07da445f8778131e2318a8fd1d45e1e2d
Chad <chawagon03@gmail.com>
2025-06-24 10:31:13 -0400

    feat(docker/mongo): add uninstall script for mongo service

---
+++ docker/mongo/uninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### GLOBAL VARIABLES
+service='mongo'
+
+### USER INPUT VARIABLES
+read -p 'SERVICE PATH [/opt/services]: ' service_path < /dev/tty
+
+### SET DEFAULT VALUES IF BLANK
+service_path=${service_path:-'/opt/services'}
+
+### STOP MONGO
+docker compose -f $service_path/$service/compose.yaml down --rmi all --volumes
+
+### MAKE MONGO DIRECTORY
+mkdir -p $service_path/$service
+
+### REMOVE MONGO SERVICE FILES/DIRECTORIES
+rm -rf $service_path/$service
