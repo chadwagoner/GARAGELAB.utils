d6f8efeb68769e62a00ff4acc6e186f769438a21
Chad <chawagon03@gmail.com>
2025-06-24 13:29:05 -0400

    fix(docker/pocket-id/install.sh): add required APP_URL input and validation to ensure proper configuration

--- docker/pocket-id/install.sh
+++ docker/pocket-id/install.sh
@@ -5,9 +5,16 @@ service='pocket-id'
 
 ### USER INPUT VARIABLES
 read -p 'SERVICE PATH [/opt/services]: ' service_path < /dev/tty
+read -p 'APP URL [REQUIRED]: ' app_url < /dev/tty
 
 ### SET DEFAULT VALUES IF BLANK
 service_path=${service_path:-'/opt/services'}
+
+### VALIDATE REQUIRED VARIABLES
+if [[ -z $app_url ]]; then
+  echo "ERROR: APP URL REQUIRED... Exiting"
+  exit 1
+fi
 
 ### MAKE POCKET-ID DIRECTORY
 mkdir -p $service_path/$service
@@ -15,11 +22,13 @@ mkdir -p $service_path/$service/data
 
 ### CREATE .ENV
 cat > $service_path/$service/.env <<EOF
-APP_URL: https://pocket-id.unicorn-stargazer.ts.net
-TRUST_PROXY: true
-PUID: 1000
-PGID: 1000
 ANALYTICS_DISABLED: true
+APP_URL: $app_url
+PGID: 1000
+PUID: 1000
+SERVICE: $service
+SERVICE_PATH: $service_path
+TRUST_PROXY: true
 EOF
 
 ### GET COMPOSE FILE
