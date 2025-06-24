a271c1e88363ff04a737a8af03182328071f334b
Chad <chawagon03@gmail.com>
2025-06-24 10:09:00 -0400

    chore(mongo): update mongo image version and improve script for dynamic service path and default values

--- docker/mongo/install.sh
+++ docker/mongo/install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+### GLOBAL VARIABLES
+service='mongo'
+
 ### USER INPUT VARIABLES
-read -p 'SERVICE PATH: ' service_path < /dev/tty
-read -p 'MONGO USERNAME: ' mongo_username < /dev/tty
-read -p 'MONGO PASSWORD: ' mongo_password < /dev/tty
+read -p 'SERVICE PATH [/opt/services]: ' service_path < /dev/tty
+read -p 'MONGO USERNAME [mongo]: ' mongo_username < /dev/tty
+read -p 'MONGO PASSWORD [changeme123]: ' mongo_password < /dev/tty
+
+### SET DEFAULT VALUES IF BLANK
+service_path=${service_path:-'/opt/services'}
+mongo_username=${mongo_username:-'mongo'}
+mongo_password=${mongo_password:-'changeme123'}
 
 ### MAKE MONGODB DIRECTORY
-mkdir -p $service_path/mongo
-mkdir -p $service_path/mongo/data
+mkdir -p $service_path/$service
+mkdir -p $service_path/$service/data
 
 ### CREATE .ENV
-echo -e "SERVICE_PATH: \"$service_path\"\nMONGO_USERNAME: \"$mongo_username\"\nMONGO_PASSWORD: \"$mongo_password\"" | tee $service_path/mongo/.env >/dev/null
+echo -e "SERVICE: \"$service\"\nSERVICE_PATH: \"$service_path\"\nMONGO_USERNAME: \"$mongo_username\"\nMONGO_PASSWORD: \"$mongo_password\"" | tee $service_path/$service/.env >/dev/null
 
 ### GET COMPOSE FILE
-curl -sL -o $service_path/mongo/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/mongo/compose.yaml
+curl -sL -o $service_path/$service/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/mongo/compose.yaml
 
 ### START MONGO
-docker compose -f $service_path/mongo/compose.yaml up -d
+docker compose -f $service_path/$service/compose.yaml up -d
