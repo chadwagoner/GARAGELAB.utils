b8f430bb57a4e6eb5860a7d0437e240af253ea04
Chad <chawagon03@gmail.com>
2025-06-25 09:56:02 -0400

    feat(docker/komodo): add uninstall and update scripts for komodo service

---
+++ docker/komodo/update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+### GLOBAL VARIABLES
+service='komodo'
+
+### USER INPUT VARIABLES
+read -p 'SERVICE PATH [/opt/services]: ' service_path < /dev/tty
+
+### SET DEFAULT VALUES IF BLANK
+service_path=${service_path:-'/opt/services'}
+
+### GET COMPOSE FILE
+curl -sL -o $service_path/$service/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/$service/compose.yaml
+
+### START SERVICE
+docker compose -f $service_path/$service/compose.yaml up -d
