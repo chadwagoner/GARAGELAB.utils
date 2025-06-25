689d26f349d4c430be2469797b5e35d9ec93e52e
Chad <chawagon03@gmail.com>
2025-06-25 09:55:14 -0400

    refactor(docker/mongo): update script commands to use generic "SERVICE" terminology instead of specific "MONGO" for better reusability and maintainability

--- docker/mongo/update.sh
+++ docker/mongo/update.sh
@@ -12,5 +12,5 @@ service_path=${service_path:-'/opt/services'}
 ### GET COMPOSE FILE
 curl -sL -o $service_path/$service/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.utils/main/docker/$service/compose.yaml
 
-### START MONGO
+### START SERVICE
 docker compose -f $service_path/$service/compose.yaml up -d
